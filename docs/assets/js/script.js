daf46eae7174e1bd1927426cc302dadcdf46257f
bgroeling <84939419+bgroeling@users.noreply.github.com>
2021-07-04 13:09:57 -0600

    Modal
    creation of the pop up bar for the settings

--- docs/assets/js/script.js
+++ docs/assets/js/script.js
@@ -1,8 +1,21 @@
 // Assignment code here
-
+// generator functions - https://www.net-comber.com/charset.html
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+// get modal
+var modal = document.getElementById("modal");
+
+// get open modal button
+var modalBtn = document.getElementById("button");
+
+// get close button
+var closeBtn = document.getElementById("close");
+
+// listen for click
+modal
+
 
 // Write password to the #password input
 function writePassword() {
@@ -10,8 +23,19 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
+}
+// Lower Case Letters
+function getRandomLower() {
+  return String.fromCharCode(console.log(Math.floor(Math.random() * 26) +97));
+}
+// Upper Case Letters
+function getRandomUpper() {
+  return String.fromCharCode(console.log(Math.floor(Math.random() * 26) +65));
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
+
+console.log(getRandomLower());
+console.log(getRandomUpper());
+
+
